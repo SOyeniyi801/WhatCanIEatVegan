4ff1e14e94f312727dcbdc14e0b7f0a14a17f874
Sammi <naturalbeauty@gmail.com>
2020-03-28 18:09:06 -0400

    hooked up js, api works in console

--- main.js
+++ main.js
@@ -1,4 +1,45 @@
 $(function () {
+    console.log("Its working")
 
-    $(".searchBar").val()
+    const url = "https://api.spoonacular.com/recipes/search?query="
+    const apiKey = "&apiKey=502be8db956a4d2581735d3ceced3501&diet=vegan"
+
+    $("button").click((event) => {
+        // event.preventDefault()
+        console.log("button pushed")
+
+        const newIngredient = $("#searchBar").val()
+        console.log(newIngredient)
+        getRecipes(newIngredient)
+
+    })
+
+    
+
+    async function getRecipes(query) {
+        const response = await axios.get(`${url}${query}${apiKey}`)
+        console.log(response.data.results)
+
+        // .map((recipe)=> { return recipe.title})
+    }
+
+    // function displayRecipes () {
+        
+    // }
+
 })
+
+
+// function displayResults (gifs) {
+//     console.log(gifs)
+//     gifs.forEach((gif) => {
+//       $('#results-table tbody').append(
+//         `<tr>
+//           <td>${gif.title}</td>
+//           <td><img src="${gif.images.fixed_height.url}"></td>
+//           <td>${gif.rating}</td>
+//           <td><a href="${gif.url}"> link </a></td>
+//         </tr>`
+//       )
+//     })
+//   }
