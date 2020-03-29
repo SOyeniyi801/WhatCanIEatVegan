5319af53c5b2cd42266f630fc4b4149a70e23fe9
Kareem Grant <kareem@getuserwise.com>
2020-03-29 18:06:50 -0400

    update logic to fix issue with mapping with promises

--- main.js
+++ main.js
@@ -8,10 +8,27 @@ $(function() {
 
     const newIngredient = $("#searchBar").val();
     console.log(newIngredient);
-    const recipes = await getRecipes(newIngredient);
-    displayRecipes(recipes)
+    // const recipes = await getRecipes(newIngredient);
+    // displayRecipes(recipes)
+    processRequest(newIngredient)
   });
-  
+
+  async function processRequest(ingredient) {
+    const recipes = await getRecipes(ingredient);
+    console.log(recipes)
+
+    // We need to use Promise.all() using map() with promises
+    // see this post: https://futurestud.io/tutorials/node-js-how-to-run-an-asynchronous-function-in-array-map
+    detailedRecipes = await Promise.all(
+      recipes.map(async(recipe) => {
+        const newRecipe = await getRecipeDetail(recipe.id)
+        return newRecipe
+      })
+    )
+
+    console.log(detailedRecipes)
+    displayRecipes(detailedRecipes)
+  }
 
   async function getRecipes(query) {
     try {
@@ -26,12 +43,12 @@ $(function() {
       });
       // const apiKey = "&apiKey=502be8db956a4d2581735d3ceced3501&diet=vegan"
       // const response = await axios.get(`${url}${query}${apiKey}`)
-      console.log(response.data.results);
-      const recipes = response.data.results
+      // console.log(response.data.results);
+      return response.data.results
 
-      return recipes.map(async(recipe) => {
-        return await getRecipeDetail(recipe.id)
-      })
+      // return recipes.map(async(recipe) => {
+      //   return await getRecipeDetail(recipe.id)
+      // })
       // console.log(response.data.results.title)
     } catch (error) {
       console.log(error);
@@ -41,20 +58,20 @@ $(function() {
 
   async function getRecipeDetail (recipeId) {
     try {
-        const url = `https://api.spoonacular.com/recipes/${recipeId}/information`;
-  
-        const response = await axios.get(url, {
-          params: {
-            apiKey: "502be8db956a4d2581735d3ceced3501",
-            includeNutrition: true
-          }
-        });
-        console.log(response.data);
-        // console.log(response.data.results.title)
-      } catch (error) {
+      const url = `https://api.spoonacular.com/recipes/${recipeId}/information`;
+
+      const response = await axios.get(url, {
+        params: {
+          apiKey: "502be8db956a4d2581735d3ceced3501",
+          includeNutrition: true
+        }
+      });
+      // console.log(response.data);
+      return response.data
+    } catch (error) {
         console.log(error);
         alert("an error has occurred, please try again");
-      }
+    }
   }
 
   function displayRecipes (recipes) {
